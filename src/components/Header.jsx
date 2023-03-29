fb2b12c3b7fcd61afb9db953de27f46b63c3f218
Diego Almeida <diegoalmeidaz@gmail.com>
2023-03-29 06:12:42 -0300

    finished home and steps

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = () => {
+const Banner1 = () => {
   return (
     <div
       className="header bg-cover bg-center h-screen flex flex-col justify-center items-center text-white relative text-center md:h-auto md:bg-blue-500 md:py-4 md:pb-8 md:px-6"
@@ -14,6 +14,66 @@ const Header = () => {
       <h6 className="text-lg font-medium text-shadow z-20">
         ¡Tu próximo evento con nueva tenida!
       </h6>
+    </div>
+  );
+};
+
+const Banner2 = () => {
+  return (
+    <div
+      className="header bg-cover bg-center h-screen flex flex-col justify-center items-center text-white relative text-center md:h-auto md:bg-blue-500 md:py-4 md:pb-8 md:px-6"
+      style={{
+        backgroundImage: `url('https://think-feel-discover.com/wp-content/uploads/2021/06/LondonFashionWeekStreetStyle2021_LFW_Dressesstyle_ThinkFeelDiscover_ChrysanthiKosmatou_FashionEditor_Blogger_Vlogger.JPG212.jpg')`,
+        height: "60vh",
+      }}
+    >
+      <div className="overlay absolute inset-0 bg-black opacity-40 z-10"></div>
+      <h1 className="text-5xl font-bold mb-4 text-shadow z-20">Rentabiliza tu Closet!</h1>
+      <h6 className="text-lg font-medium text-shadow z-20">
+        Gana dinero con nuestro club
+      </h6>
+      <a
+        href="/register"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 z-20"
+      >
+        Regístrate
+      </a>
+    </div>
+  );
+};
+
+const Banner3 = () => {
+  return (
+    <div
+      className="header bg-cover bg-center h-screen flex flex-col justify-center items-center text-white relative text-center md:h-auto md:bg-blue-500 md:py-4 md:pb-8 md:px-6"
+      style={{
+        backgroundImage: `url('https://media.istockphoto.com/id/1295901898/photo/young-black-woman-with-curly-hair-in-yellow-dress-and-with-styles-attitude-laughing-happy.jpg?s=612x612&w=0&k=20&c=1TyAV7zs-mqE8CadNeuisB7h78r4KYJAQROlGdowva8=')`,
+        height: "60vh",
+      }}
+    >
+      <div className="overlay absolute inset-0 bg-black opacity-40 z-10"></div>
+      <ul className="space-y-2 z-20">
+        <li className="text-xl font-bold text-shadow">Prendas únicas</li>
+        <li className="text-xl font-bold text-shadow">Telas diferentes</li>
+        <li className="text-xl font-bold text-shadow">Moda Circular</li>
+        <li className="text-xl font-bold text-shadow">Lujo Accesible</li>
+      </ul>
+      <a
+        href="/dresses"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 z-20"
+      >
+        Revisa nuestro catálogo de vestidos
+      </a>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <Banner1 />
+      <Banner2 />
+      <Banner3 />
     </div>
   );
 };
--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,59 +1,79 @@
 import React from "react";
-import Step1 from "../assets/img/Steps/Paso1.PNG";
-import Step2 from "../assets/img/Steps/Paso2.PNG";
-import Step3 from "../assets/img/Steps/Paso3.PNG";
-import Step4 from "../assets/img/Steps/Paso4.PNG";
-import Step5 from "../assets/img/Steps/Paso5.PNG";
+import { FaCalendarAlt, FaTshirt, FaCar, FaEnvelope, FaTruckPickup } from "react-icons/fa";
 
-
-
-
-
-const MiniLanding = () => {
-    return (
-    <div className="grid grid-cols-2 md:grid-cols-5 md:grid-rows-2 gap-4 h-full">
-      <div className="relative h-full col-span-2">
-        <img
-          src={Step1}
-          alt="Paso 1"
-          className="w-full h-full object-fit"
-        />
-        <div className="absolute inset-0 border-2 border-white shadow-lg"></div>
-      </div>
-      <div className="relative h-full col-span-2">
-        <img
-          src={Step2}
-          alt="Paso 2"
-          className="w-full h-full object-fit"
-        />
-        <div className="absolute inset-0 border-2 border-white shadow-lg"></div>
-      </div>
-      <div className="relative h-full col-span-2">
-        <img
-          src={Step3}
-          alt="Paso 3"
-          className="w-full h-full object-fit"
-        />
-        <div className="absolute inset-0 border-2 border-white shadow-lg"></div>
-      </div>
-      <div className="relative h-full col-span-2 row-span-1">
-        <img
-          src={Step4}
-          alt="Paso 4"
-          className="w-full h-full object-fit"
-        />
-        <div className="absolute inset-0 border-2 border-white shadow-lg"></div>
-      </div>
-      <div className="relative h-full col-span-2">
-        <img
-          src={Step5}
-          alt="Paso 5"
-          className="w-full h-full object-fit"
-        />
-        <div className="absolute inset-0 border-2 border-white shadow-lg"></div>
+const HowItWorks = () => {
+  return (
+    <div className="px-4 py-8 md:px-12 md:py-16">
+      <h2 className="text-center text-3xl font-bold mb-8 text-red-500">Cómo funciona</h2>
+      <div className="flex flex-col space-y-4">
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <div className="flex-1">
+            <div className="flex items-center space-x-4">
+              <FaCalendarAlt className="text-3xl text-red-500" />
+              <h3 className="text-xl font-bold">Paso 1</h3>
+            </div>
+            <p className="mt-2">
+              Agenda tu visita a nuestro showroom para que te pruebes lo que te gusta sin costo alguno.
+            </p>
+          </div>
+        </div>
+        <hr className="border-t border-pink-100" />
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <div className="flex-1">
+            <div className="flex items-center space-x-4">
+              <FaTshirt className="text-3xl text-red-500" />
+              <h3 className="text-xl font-bold">Paso 2</h3>
+            </div>
+            <p className="mt-2">
+              Elige la tenida que te gusta y resérvala para la fecha de tu evento.
+              <br />
+              <strong>*Para reservar tendrás que abonar el valor de arriendo + la garantía*</strong>
+            </p>
+          </div>
+        </div>
+        <hr className="border-t border-pink-100" />
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <div className="flex-1">
+            <div className="flex items-center space-x-4">
+              <FaCar className="text-3xl text-red-500" />
+              <h3 className="text-xl font-bold">Paso 3</h3>
+            </div>
+            <p className="mt-2">
+              Retira tu tenida el día previamente coordinado y sé la más guapa del evento!
+            </p>
+          </div>
+        </div>
+        <hr className="border-t border-pink-100" />
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <div className="flex-1">
+            <div className="flex items-center space-x-4">
+              <FaEnvelope className="text-3xl text-red-500" />
+              <h3 className="text-xl font-bold">Paso 4</h3>
+            </div>
+            <p className="mt-2">
+              Devuelve la tenida el día domingo o lunes am y el martes de devolvemos la garantía.
+              <br />
+              <strong>*Nosotras nos encargamos de la tintorería*</strong>
+            </p>
+          </div>
+        </div>
+        <hr className="border-t border-pink-100" />
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <div className="flex-1">
+            <div className="flex items-center space-x-4">
+              <FaTruckPickup className="text-3xl text-red-500" />
+              <h3 className="text-xl font-bold">Paso 5</h3>
+            </div>
+            <p className="mt-2">
+              Recuerda que si quieres, nosotras podemos pasar a retirar la tenida con previa coordinación.
+              <br />
+              Este servicio tiene un costo extra de $4.000.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default MiniLanding;
+export default HowItWorks;
